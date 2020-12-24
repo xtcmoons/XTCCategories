bf1224476690d47848f80576c8a12349ec5f1e7b
陈明 <chenming2@lppz.com>
2020-12-24 10:16:08 +0800

    add iphone 12 machine

--- Categories/UIKit/UIDevice+XTCAdd.m
+++ Categories/UIKit/UIDevice+XTCAdd.m
@@ -87,8 +87,10 @@
                               @"iPhone12,1" : @"iPhone 11",
                               @"iPhone12,3" : @"iPhone 11 Pro",
                               @"iPhone12,5" : @"iPhone 11 Pro Max",
+                              @"iPhone13,1" : @"iPhone 12 Mini",
                               @"iPhone13,2" : @"iPhone 12",
                               @"iPhone13,3" : @"iPhone 12 Pro",
+                              @"iPhone13,4" : @"iPhone 12 Pro Max",
 
                               @"iPad1,1" : @"iPad 1",
                               @"iPad2,1" : @"iPad 2 (WiFi)",
